9759b4f1bca9e696744f90c1ec7dfb7a57adb912
anna pergher <e227275@edu.sbl.ch>
2024-10-23 13:56:30 +0000

    aufgabe 3

--- script.js
+++ script.js
@@ -56,3 +56,19 @@ export function aufgabe02(args) {
 }
 
 linkupExerciseHandler("[data-click=aufgabe02]", aufgabe02)
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
+  }
+  return count
+}
+linkupExerciseHandler("[data-click=aufgabe03]", aufgabe03)
