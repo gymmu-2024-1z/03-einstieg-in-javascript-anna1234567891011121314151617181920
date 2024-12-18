1266df2e6e8cb4ecfe5581140ed8fde322c7fd40
anna pergher <e227275@edu.sbl.ch>
2024-12-18 16:01:28 +0000

    Aufgabe 17 mit Hilfe des Videos gelöst

--- script.js
+++ script.js
@@ -325,6 +325,27 @@ export function aufgabe16(args) {
 }
 linkupExerciseHandler("[data-click=aufgabe16]", aufgabe16)
 
+export function aufgabe17(args) {
+  const input = args
+  const totalList = []
+  const currentList = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    // Wenn wir auf ein Leerzeichen treffen, dann schreiben wir alles was wir bis jetzt haben, in die totalList
+
+    if (currentElement === " ") {
+      totalList.push(currentList.join(""))
+      currentList.length = 0
+    } else {
+      currentList.push(currentElement)
+    }
+  }
+  //Wir schreiben alles was wir noch bis zum Ende gelesen haben, in die Liste
+  totalList.push(currentList.join(""))
+  return totalList
+}
+linkupExerciseHandler("[data-click=aufgabe17]", aufgabe17)
+
 export function aufgabe18(args) {
   const input = args
   const result = []
